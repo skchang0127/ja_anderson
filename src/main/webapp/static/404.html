63103a8c8a79a0939d4da20bfe8d31cff7b6bde5
司晓 <shaokun.zsk@alipay.com>
2016-04-01 18:21:56 +0800

    import antd

--- src/main/webapp/static/404.html
+++ src/main/webapp/static/404.html
@@ -8,7 +8,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>404</title>
 </head>
 <body>
 
---
+++ src/main/webapp/static/src/component/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Datepicker } from 'antd';
+
+const App = React.createClass({
+  render() {
+    return <div>
+      <Datepicker />
+    </div>;
+  },
+});
+
+export default App;
